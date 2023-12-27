48c5e8a0d1026c96bba22748b409614ab70ef85a
bianavic <bianavic@gmail.com>
2023-12-27 13:21:03 -0300

    initial commit

---
+++ src/test/kotlin/org/edu/fabs/creditrequestsystem/CreditRequestSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.edu.fabs.creditrequestsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditRequestSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
