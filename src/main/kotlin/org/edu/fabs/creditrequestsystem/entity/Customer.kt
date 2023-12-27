7e8fa9b19af2b87b34ade8dac7c45a8672dde02d
bianavic <bianavic@gmail.com>
2023-12-27 14:50:27 -0300

    feat: update application file to create database tables

--- src/main/kotlin/org/edu/fabs/creditrequestsystem/entity/Customer.kt
+++ src/main/kotlin/org/edu/fabs/creditrequestsystem/entity/Customer.kt
@@ -12,7 +12,7 @@ data class Customer(
     @Column(nullable = false, unique = true) var email: String = "",
     @Column(nullable = false) var password: String = "",
     @Column(nullable = false) @Embedded var address: Address = Address(),
-    // ao carregar o Customer nao carrega a lista de rcedito dele, carrega apenas se fizer esta solicitacao
+    // ao carregar o Customer nao carrega a lista de credito dele, carrega apenas se fizer esta solicitacao -> por isso primeiro nao aparece essa coluna no primeiro acesso ao h2-console
     @Column(nullable = false) @OneToMany(fetch = FetchType.LAZY,
         cascade = arrayOf(CascadeType.ALL, CascadeType.PERSIST),
         mappedBy = "customer") var credits: List<Credit> = mutableListOf(),
