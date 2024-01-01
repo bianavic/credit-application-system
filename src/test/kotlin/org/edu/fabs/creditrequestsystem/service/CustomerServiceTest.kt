5d55802c06608b7bc70fc4b4f615df4de76cd2b4
bianavic <bianavic@gmail.com>
2024-01-01 10:42:32 -0300

    test(service): create unit test to customer

---
+++ src/test/kotlin/org/edu/fabs/creditrequestsystem/service/CustomerServiceTest.kt
@@ -0,0 +1,113 @@
+package org.edu.fabs.creditrequestsystem.service
+
+import io.mockk.*
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import org.assertj.core.api.Assertions
+import org.edu.fabs.creditrequestsystem.entity.Address
+import org.edu.fabs.creditrequestsystem.entity.Customer
+import org.edu.fabs.creditrequestsystem.exception.BusinessException
+import org.edu.fabs.creditrequestsystem.repository.CustomerRepository
+import org.edu.fabs.creditrequestsystem.service.impl.CustomerServiceImpl
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import java.math.BigDecimal
+import java.util.*
+
+@ExtendWith(MockKExtension::class)
+class CustomerServiceTest {
+
+    @MockK
+    lateinit var customerRepository: CustomerRepository
+
+    @InjectMockKs
+    lateinit var customerServiceImpl: CustomerServiceImpl
+
+    @AfterEach
+    fun tearDown() {
+        clearAllMocks()
+    }
+
+    @Test
+    fun `should create customer`() {
+        //given
+        val fakeCustomer: Customer = buildCustomer()
+        every { customerRepository.save(any()) } returns fakeCustomer
+        //when
+        val actual: Customer = customerServiceImpl.save(fakeCustomer)
+        //then
+        Assertions.assertThat(actual).isNotNull
+        Assertions.assertThat(actual).isSameAs(fakeCustomer)
+        verify(exactly = 1) { customerRepository.save(fakeCustomer) }
+    }
+
+    @Test
+    fun `should find customer by id`() {
+        //given
+        val fakeId: Long = Random().nextLong()
+        val fakeCustomer: Customer = buildCustomer(id = fakeId)
+        every { customerRepository.findById(fakeId) } returns Optional.of(fakeCustomer)
+        //when
+        val actual: Customer = customerServiceImpl.findById(fakeId)
+        //then
+        Assertions.assertThat(actual).isNotNull
+        Assertions.assertThat(actual).isExactlyInstanceOf(Customer::class.java)
+        Assertions.assertThat(actual).isSameAs(fakeCustomer)
+        verify(exactly = 1) { customerRepository.findById(fakeId) }
+    }
+
+    @Test
+    fun `should not find customer by invalid id and throw BusinessException`() {
+        //given
+        val fakeId: Long = Random().nextLong()
+        every { customerRepository.findById(fakeId) } returns Optional.empty()
+        //when
+        //then
+        Assertions.assertThatExceptionOfType(BusinessException::class.java)
+            .isThrownBy { customerServiceImpl.findById(fakeId) }
+            .withMessage("Id $fakeId not found")
+        verify(exactly = 1) { customerRepository.findById(fakeId) }
+    }
+
+    @Test
+    fun `should delete customer by id`() {
+        //given
+        val fakeId: Long = Random().nextLong()
+        val fakeCustomer: Customer = buildCustomer(id = fakeId)
+        every { customerRepository.findById(fakeId) } returns Optional.of(fakeCustomer)
+        every { customerRepository.delete(fakeCustomer) } just runs
+        //when
+        customerServiceImpl.delete(fakeId)
+        //then
+        verify(exactly = 1) { customerRepository.findById(fakeId) }
+        verify(exactly = 1) { customerRepository.delete(fakeCustomer) }
+    }
+
+    companion object {
+        fun buildCustomer(
+            firstName: String = "First Name 1",
+            lastName: String = "Last Name 1",
+            cpf: String = "28475934625",
+            email: String = "test1@gmail.com",
+            password: String = "1111",
+            zipCode: String = "1111",
+            street: String = "Street 1",
+            income: BigDecimal = BigDecimal.valueOf(1000.0),
+            id: Long = 1L
+        ) = Customer(
+            firstName = firstName,
+            lastName = lastName,
+            cpf = cpf,
+            email = email,
+            password = password,
+            address = Address(
+                zipCode = zipCode,
+                street = street,
+            ),
+            income = income,
+            id = id
+        )
+    }
+}
